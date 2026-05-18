9cf3049ccc00d6cad99b75d6aef5b89b325e81ee
vitasok797 <vitasok@gmail.com>
2026-05-19 00:58:21 +0300

    [lib_tests] test: Update vincentlaucsb-csv-parser library test

--- lib_tests/lib_tests/vincentlaucsb-csv-parser.cpp
+++ lib_tests/lib_tests/vincentlaucsb-csv-parser.cpp
@@ -6,27 +6,37 @@
 #include <string>
 #include <string_view>
 
-static auto test_file(const std::string& path) -> void
+static auto test_file(int header_row, const std::string& path, const char* expected_res) -> void
 {
     auto format = csv::CSVFormat{};
     format.delimiter({';', ','});
-    format.header_row(0);
+    format.header_row(header_row);
     format.trim({' ', '\t'});
 
     auto csv = csv::CSVReader{path, format};
 
-    auto header_content = std::string{};
-    for (const auto& col_header : csv.get_col_names())
+    if (header_row >= 0)
     {
-        header_content += "[";
-        header_content += col_header;
-        header_content += "]";
+        auto header_content = std::string{};
+        for (const auto& col_header : csv.get_col_names())
+        {
+            header_content += "[";
+            header_content += col_header;
+            header_content += "]";
+        }
+        ASSERT_EQ(header_content, "[col1][col2]");
     }
-    ASSERT_EQ(header_content, "[col1][col2]");
 
     auto body_content = std::string{};
     for (const auto& row : csv)
     {
+        auto is_single_empty_cell = (row.size() == 1 && row[0].is_null());
+        if (is_single_empty_cell)
+        {
+            body_content += "*|";
+            continue;
+        }
+
         ASSERT_EQ(row.size(), 2);
         for (auto& cell : row)
         {
@@ -36,14 +46,25 @@ static auto test_file(const std::string& path) -> void
         }
         body_content += "|";
     }
-    ASSERT_EQ(body_content, "[val1][001]|[val2][002]|[val3][003]|");
+    ASSERT_EQ(body_content, expected_res);
 }
 
-TEST(VincentlaucsbCsvParserTest, Complex)
+TEST(VincentlaucsbCsvParserTest, WithHeader)
 {
     const auto data_dir = std::string{"lib_tests/data/csv"};
+    const auto expected_res = "[val1][001]|[val2][002]|[val3][003]|";
 
-    ASSERT_NO_FATAL_FAILURE(test_file(data_dir + "/data1_lf.csv"));
-    ASSERT_NO_FATAL_FAILURE(test_file(data_dir + "/data2_crlf.csv"));
-    ASSERT_NO_FATAL_FAILURE(test_file(data_dir + "/data3_utf8bom_lf.csv"));
+    ASSERT_NO_FATAL_FAILURE(test_file(0, data_dir + "/data1_lf.csv", expected_res));
+    ASSERT_NO_FATAL_FAILURE(test_file(0, data_dir + "/data2_crlf.csv", expected_res));
+    ASSERT_NO_FATAL_FAILURE(test_file(0, data_dir + "/data3_utf8bom_lf.csv", expected_res));
+}
+
+TEST(VincentlaucsbCsvParserTest, WithoutHeader)
+{
+    const auto data_dir = std::string{"lib_tests/data/csv"};
+    const auto expected_res = "[col1][col2]|[val1][001]|*|[val2][002]|[val3][003]|";
+
+    ASSERT_NO_FATAL_FAILURE(test_file(-1, data_dir + "/data1_lf.csv", expected_res));
+    ASSERT_NO_FATAL_FAILURE(test_file(-1, data_dir + "/data2_crlf.csv", expected_res));
+    ASSERT_NO_FATAL_FAILURE(test_file(-1, data_dir + "/data3_utf8bom_lf.csv", expected_res));
 }
