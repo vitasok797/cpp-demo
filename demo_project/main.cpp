4a81ec9c0504927c290ca8e6c491906fdf0025de
vitasok797 <vitasok@gmail.com>
2026-09-07 22:23:42 +0300

    Add minimal Crow demo

--- demo_project/main.cpp
+++ demo_project/main.cpp
@@ -1,2 +1,11 @@
+#include <./crow.h>
+
 auto main() -> int
-{}
+{
+    auto app = crow::SimpleApp{};
+
+    CROW_ROUTE(app, "/")([]() { return "Hello from Crow"; });
+
+    // http://localhost:18080/
+    app.port(18080).multithreaded().run();
+}
