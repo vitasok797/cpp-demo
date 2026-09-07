d969401db5322a7362b44ddb1735803706fcabc4
vitasok797 <vitasok@gmail.com>
2026-09-08 00:32:45 +0300

    Add minimal Qt demo

--- demo_project/main.cpp
+++ demo_project/main.cpp
@@ -1,2 +1,23 @@
-auto main() -> int
-{}
+#include <./QApplication>
+#include <./QPushButton>
+#include <./QVBoxLayout>
+#include <./QWidget>
+
+auto main(int argc, char* argv[]) -> int
+{
+    auto app = QApplication(argc, argv);
+
+    auto window = QWidget{};
+    window.setWindowTitle("Minimal Qt app");
+    window.resize(320, 240);
+
+    auto* layout = new QVBoxLayout(&window);
+    auto* button = new QPushButton("Close app", &window);
+
+    layout->addWidget(button);
+
+    QObject::connect(button, &QPushButton::clicked, &app, &QApplication::quit);
+
+    window.show();
+    return app.exec();
+}
